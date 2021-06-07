93ddccdc1aceecdeafbd050c33c1288607d8ab8a
Binkita Tiwari <135st@BINTIWAR-MPUBX.cisco.com>
2021-06-07 15:58:31 +0530

    initial commit for package my-tree

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'my-tree'
+import Tree from 'my-tree'
 import 'my-tree/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Tree />
 }
 
 export default App
